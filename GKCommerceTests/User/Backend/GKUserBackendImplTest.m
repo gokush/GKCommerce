5002f3933c2b2c6c954aac2f8bff1bbf363cf653
goku <kakarrot.sh@qq.com>
2015-01-19 22:31:55 +0800

    User unittest

--- GKCommerceTests/User/Backend/GKUserBackendImplTest.m
+++ GKCommerceTests/User/Backend/GKUserBackendImplTest.m
@@ -7,13 +7,42 @@
 //
 
 #import <Specta/Specta.h>
+#define EXP_SHORTHAND
+#import <Expecta/Expecta.h>
 #import "Dependency.h"
+
+#import <XCTest/XCTest.h>
 
 SpecBegin(GKUserBackendImpl)
 
 describe(@"GKUserBackendImpl", ^{
   it(@"should do user info", ^{
-    
+
   });
 });
 SpecEnd
+
+
+@interface GKUserBackendImpl : XCTestCase
+@end
+
+@implementation GKUserBackendImpl
+
+- (void)testAuthenticate
+{
+  id<GKUserBackend> backend = [[Dependency shared] userBackend];
+  UserAuthenticationModel *authentication;
+  authentication = [[UserAuthenticationModel alloc]
+                    initWithUsername:@"swagger" password:@"swagger"];
+  NSLog(@"here");
+  [[backend requestAuthenticate:authentication] subscribeNext:^(User *user) {
+    [[backend requestUser:user] subscribeNext:^(User *user) {
+      expect(@"swagger").to.equal(@"swagger");
+    } error:^(NSError *error) {
+      NSLog(@"%@", error.localizedDescription);
+    }];
+  } error:^(NSError *error) {
+    NSLog(@"%@", error.localizedDescription);
+  }];
+}
+@end
