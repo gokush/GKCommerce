6cdccbaf88ee3e6899d94417affbfcac1b966141
goku <kakarrot.sh@qq.com>
2015-03-30 20:54:35 +0800

    地址选择

---
+++ GKCommerce/GKCommerce/Checkout/TableViewCell/CheckoutChoiceAddressTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  CheckoutChoiceAddressTableViewCell.h
+//  GKCommerce
+//
+//  Created by 小悟空 on 3/30/15.
+//  Copyright (c) 2015 GKCommerce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckoutChoiceAddressTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *phoneNumberLabel;
+@property (strong, nonatomic) IBOutlet UILabel *addressLabel;
+@end
---
+++ GKCommerce/GKCommerce/Checkout/TableViewCell/CheckoutChoiceAddressTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CheckoutChoiceAddressTableViewCell.m
+//  GKCommerce
+//
+//  Created by 小悟空 on 3/30/15.
+//  Copyright (c) 2015 GKCommerce. All rights reserved.
+//
+
+#import "CheckoutChoiceAddressTableViewCell.h"
+
+@implementation CheckoutChoiceAddressTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
